a42df30be272a73f1dda3d0f7dbbff16a6d94826
Ganesh Narayanan <ganeshncsu@gmail.com>
2021-09-23 16:10:10 -0400

    updated matplotlib figures
    matplotlib figure

---
+++ Scripts/Matplotlib-trials.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+merged_df = pd.read_csv("~/playground/clinical-trials/Output/merged_df.csv")
+print(merged_df["Continent_Name"].value_counts())
+
+plt.style.use("fivethirtyeight")
+slices = merged_df["Continent_Name"].value_counts()
+labels = ['North America',"Europe","Asia","South America","Africa","Oceania"]
+colors = ["#008fd5","#fc4f30","#e5ae37","#6d904f","#D02090","#308014"]
+explode = [0.1, 0, 0, 0, 0.2, 0]
+
+plt.pie(slices, labels = labels,explode = explode,
+        startangle = -15,autopct = "%1.0f%%",
+        colors = colors,
+        wedgeprops = {"edgecolor":"black"})
+plt.title("Clinical Trials on Skin Regeneration Across the World",fontsize = 20)
+plt.tight_layout()
+plt.savefig("Pie Chart without Shadow.jpg")
+plt.show()
+
+
--- Scripts/clinical_trials_data_analyses.py
+++ Scripts/clinical_trials_data_analyses.py
@@ -6,7 +6,6 @@ ct = ClinicalTrials()
 # importing country to continent data
 country_continent = pd.read_csv("clinical-trials/Input/country-and-continent-codes-list.csv")
 
-
 def fetch_country(ctr):
     country = ctr.split("|")
     country_name = country[0]
@@ -126,7 +125,11 @@ if __name__ == "__main__":
     frames = [skhealing_diabetes(),skin_hydrogel(),wound_heal_hydrogel(),wound_hydrogel(),skin_fibers(),
                           wound_heal_fibers(),skin_heal_gels(),wound_heal_gels(),skin_foams(),wound_foams()]
     clinical_trials_data_df = pd.concat(frames)
+    clinical_trials_data_df.drop_duplicates(keep=False, inplace=True)
     clinical_trials_data_df["Country_Name"] = clinical_trials_data_df["LocationCountry"].apply(lambda x : fetch_country(x))
-    # merged_df = clinical_trials_data_df.merge(country_continent, how="inner", on="Country_Name")
-    clinical_trials_data_df.to_csv("~/playground/clinical-trials/Output/clinical_trials_data.csv")
+    clinical_trials_data_df = clinical_trials_data_df.drop(["LocationCountry"],axis=1)
+    merged_df = clinical_trials_data_df.merge(country_continent, how="inner", on="Country_Name").drop(
+        ["Three_Letter_Country_Code","Country_Number"], axis = 1)
+    merged_df.to_csv("~/playground/clinical-trials/Output/merged_df.csv")
+    print(merged_df["InterventionType"].value_counts())
     print('success')
